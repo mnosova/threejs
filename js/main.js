e91c52b52d9207914addf7f966f1438c33507069
Marina Nosova <nosova_marino4ka@mail.ru>
2019-07-30 08:32:39 +0300

    done moving

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ window.onload = () => {
         camera.updateProjectionMatrix();
         renderer.setSize(window.innerWidth, window.innerHeight);
     }
+
     //базовые переменные
     let canvas = document.getElementById('canvas');
     let renderer = new THREE.WebGLRenderer({canvas: canvas});
@@ -36,9 +37,9 @@ window.onload = () => {
         }
         let positon = 0;
         const rand = Math.floor(Math.random() * array.length);
-        positon=array[rand];
+        positon = array[rand];
         array.splice(rand, 1);
-        if(positon){
+        if (positon) {
             return positon;
         }
         if (!array.length) {
@@ -94,180 +95,74 @@ window.onload = () => {
 
     //вывод элеменов
 
-    // let start = {
-    //     positionX: getRandomItem(startX),
-    //     positionY: getRandomItem(startY),
-    //     direction : getRandomItem(direct)
-    //
-    // };
 
-    // let number = 5;
-    //let d= [];
-    let mesh_1;
-    let mesh_2;
-    let mesh_3;
-    let mesh_4;
-    let mesh_5;
-    // for(let i=1; i<=number;i++){
-        mesh_1 = new THREE.Mesh(geometry, material);
-        mesh_1.name = getRandomItem(direct);
-        mesh_1.position.x = getRandomItem(startX);
-        mesh_1.position.y = getRandomItem(startY);
-        //d.push('direction_'+i, getRandomItem(direct));
-    mesh_2 = new THREE.Mesh(geometry, material);
-    mesh_2.name = getRandomItem(direct);
-    mesh_2.position.x = getRandomItem(startX);
-    mesh_2.position.y = getRandomItem(startY);
+    let number = 5;
+    let elements = [];
+    for (let i = 1; i <= number; i++) {
+        let mesh = new THREE.Mesh(geometry, material);
+        mesh.name = `mesh_${i}`;
+        mesh.position.x = getRandomItem(startX);
+        mesh.position.y = getRandomItem(startY);
+        mesh.direction = getRandomItem(direct);
+        elements.push(mesh);
 
-    mesh_3 = new THREE.Mesh(geometry, material);
-    mesh_3.name = getRandomItem(direct);
-    mesh_3.position.x = getRandomItem(startX);
-    mesh_3.position.y = getRandomItem(startY);
-
-    mesh_4 = new THREE.Mesh(geometry, material);
-    mesh_4.name = getRandomItem(direct);
-    mesh_4.position.x = getRandomItem(startX);
-    mesh_4.position.y = getRandomItem(startY);
-
-    mesh_5 = new THREE.Mesh(geometry, material);
-    mesh_5.name = getRandomItem(direct);
-    mesh_5.position.x = getRandomItem(startX);
-    mesh_5.position.y = getRandomItem(startY);
-
-
-
-
-    // }
-
-    scene.add(mesh_1);
-    scene.add(mesh_2);
-    scene.add(mesh_3);
-    scene.add(mesh_4);
-    scene.add(mesh_4);
-
-
-
-
-    // let mesh = new THREE.Mesh(geometry, material);
-    // mesh.position.x = start.positionX;
-    // mesh.position.y = start.positionY;
-    // mesh.name = `mesh_1`;
-    //let direction = start.direction;
-    // scene.add(mesh.na(`mesh_1`));
-
-    // let mesh2 = new THREE.Mesh(geometry, material);
-    // mesh.position.x = start.positionX;
-    // mesh.position.y = start.positionY;
-    //let direction2 = start.direction;
-
-    //scene.add(mesh2);
-    
+        scene.add(mesh);
+    }
 
 
     function loop() {
-        //касание правой
-        // if (Math.round(mesh.position.x) >= window.innerWidth / 2 || mesh.name === 'left') {
-        //     mesh.position.x = (window.innerWidth / 2)-3;
-        //     mesh.name = 'left';
-        // }
-        // //касание левой
-        // if (Math.round(mesh.position.x) <= -window.innerWidth / 2 || mesh.name === 'right') {
-        //     mesh.position.x = (-window.innerWidth / 2)+3;
-        //     mesh.name = 'right';
-        // }
-        // //касание низа
-        // if (Math.round(mesh.position.y) >= window.innerHeight / 2 || mesh.name === 'top') {
-        //     mesh.position.y = (window.innerHeight / 2) -3;
-        //     mesh.name = 'top';
-        // }
-        // //касание верха
-        // if (Math.round(mesh.position.y) <= -window.innerHeight / 2 || mesh.name === 'bottom') {
-        //     mesh.position.y = (-window.innerHeight / 2)+3;
-        //     mesh.name = 'bottom';
-        // }
-        //
-        // //касание правой
-        // if (mesh.position.x === (window.innerWidth / 2)-3 || mesh.name === 'left') {
-        //     mesh.rotation.y += -steps.rotationY;
-        //     mesh.rotation.z += -steps.rotationZ;
-        //     mesh.position.x -= steps.positionX;
-        //     mesh.position.y -= steps.positionY;
-        //     //касание левой
-        // }
-        // if (mesh.position.x === (-window.innerWidth / 2)+3 || mesh.name === 'right') {
-        //     mesh.rotation.y += -steps.rotationY;
-        //     mesh.rotation.z += -steps.rotationZ;
-        //     mesh.position.x += steps.positionX;
-        //     mesh.position.y += steps.positionY;
-        // }
-        // //касание верха
-        // if (mesh.position.y ===(window.innerHeight / 2) -3 || mesh.name === 'top') {
-        //     mesh.rotation.y += -steps.rotationY;
-        //     mesh.rotation.z += -steps.rotationZ;
-        //     mesh.position.x += steps.positionX;
-        //     mesh.position.y -= steps.positionY * 2;
-        //     //касание низа
-        // }
-        // if (mesh.position.y === (-window.innerHeight / 2)+3 || mesh.name === 'bottom') {
-        //     mesh.rotation.y += -steps.rotationY;
-        //     mesh.rotation.z += -steps.rotationZ;
-        //     mesh.position.x += -steps.positionX;
-        //     mesh.position.y += steps.positionY * 2;
-        // }
-        console.log(mesh);
+        for (let i = 0; i <= elements.length - 1; i++) {
+            //касание правой
+            if (elements[i].position.x >= window.innerWidth / 2) {
+                elements[i].position.x = (window.innerWidth / 2) - 3;
+                elements[i].direction = 'left';
+            }
+            //касание левой
+            if (elements[i].position.x <= -window.innerWidth / 2 ) {
+                elements[i].position.x = (-window.innerWidth / 2) + 3;
+                elements[i].direction = 'right';
+            }
+            //касание низа
+            if (elements[i].position.y >= window.innerHeight / 2 ) {
+                elements[i].position.y = (window.innerHeight / 2) - 3;
+                elements[i].direction = 'top';
+            }
+            //касание верха
+            if (elements[i].position.y <= -window.innerHeight / 2) {
+                elements[i].position.y = (-window.innerHeight / 2) + 3;
+                elements[i].direction = 'bottom';
+            }
 
+            //касание правой
+            if (elements[i].direction === 'left') {
+                elements[i].rotation.y += -steps.rotationY;
+                elements[i].rotation.z += -steps.rotationZ;
+                elements[i].position.x -= steps.positionX;
+                elements[i].position.y -= steps.positionY;
+                //касание левой
+            }
+            if (elements[i].direction === 'right') {
+                elements[i].rotation.y += -steps.rotationY;
+                elements[i].rotation.z += -steps.rotationZ;
+                elements[i].position.x += steps.positionX;
+                elements[i].position.y += steps.positionY;
+            }
+            //касание верха
+            if (elements[i].direction === 'top') {
+                elements[i].rotation.y += -steps.rotationY;
+                elements[i].rotation.z += -steps.rotationZ;
+                elements[i].position.x += steps.positionX;
+                elements[i].position.y -= steps.positionY;
+                //касание низа
+            }
+            if (elements[i].direction === 'bottom') {
+                elements[i].rotation.y += -steps.rotationY;
+                elements[i].rotation.z += -steps.rotationZ;
+                elements[i].position.x += -steps.positionX;
+                elements[i].position.y += steps.positionY;
+            }
 
-        ///////////////////////////
-
-        //касание правой
-        // if (Math.round(mesh.position.x) >= window.innerWidth / 2 || direction === 'left') {
-        //     start.positionX = window.innerWidth / 2;
-        //     direction = 'left';
-        // }
-        // //касание левой
-        // if (Math.round(mesh.position.x) <= -window.innerWidth / 2 || direction === 'right') {
-        //     start.positionX = -window.innerWidth / 2;
-        //     direction = 'right';
-        // }
-        // //касание низа
-        // if (Math.round(mesh.position.y) >= window.innerHeight / 2 || direction === 'top') {
-        //     start.positionY = window.innerHeight / 2;
-        //     direction = 'top';
-        // }
-        // //касание верха
-        // if (Math.round(mesh.position.y) <= -window.innerHeight / 2 || direction === 'bottom') {
-        //     start.positionY = -window.innerHeight / 2;
-        //     direction = 'bottom';
-        // }
-        //
-        // //касание правой
-        // if (start.positionX === window.innerWidth / 2) {
-        //     mesh.rotation.y += -steps.rotationY;
-        //     mesh.rotation.z += -steps.rotationZ;
-        //     mesh.position.x -= steps.positionX;
-        //     mesh.position.y -= steps.positionY;
-        //     //касание левой
-        // }
-        // if (start.positionX === -window.innerWidth / 2) {
-        //     mesh.rotation.y += -steps.rotationY;
-        //     mesh.rotation.z += -steps.rotationZ;
-        //     mesh.position.x += steps.positionX;
-        //     mesh.position.y += steps.positionY;
-        // }
-        // //касание верха
-        // if (start.positionY === window.innerHeight / 2) {
-        //     mesh.rotation.y += -steps.rotationY;
-        //     mesh.rotation.z += -steps.rotationZ;
-        //     mesh.position.x += steps.positionX;
-        //     mesh.position.y -= steps.positionY * 2;
-        //     //касание низа
-        // }
-        // if (start.positionY === -window.innerHeight / 2) {
-        //     mesh.rotation.y += -steps.rotationY;
-        //     mesh.rotation.z += -steps.rotationZ;
-        //     mesh.position.x += -steps.positionX;
-        //     mesh.position.y += steps.positionY * 2;
-        // }
+        }
 
         renderer.render(scene, camera);
         requestAnimationFrame(function () {
